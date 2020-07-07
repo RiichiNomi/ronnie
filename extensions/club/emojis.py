c61c8186657f7417089e91809a19941de63dd33c
Oscar Feng <oscarfzs@MacBook-Pro.fios-router.home>
2020-07-07 16:02:57 -0400

    cleared extensions folder

--- extensions/club/emojis.py
+++
@@ -1,7 +0,0 @@
-#list of emojis to  be displayed for orphans of the week/month
-role_emojis = {
-    'ORPHAN OF THE WEEK':':trophy:',
-    'ORPHANS OF THE MONTH':':rosette:',
-    'Club Yakuman': ':cherry_blossom:',
-    'Orphanage Schoolmasters': ':book:'
-}
