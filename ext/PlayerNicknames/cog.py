a21dbeef4ec34b52a4032048ff6d6e461d49cebf
Joshua Kwan <joshk@triplehelix.org>
2021-02-25 05:07:34 +0000

    use json for PlayerNicknames to remove pandas dependency

--- ext/PlayerNicknames/cog.py
+++ ext/PlayerNicknames/cog.py
@@ -1,44 +1,38 @@
 import asyncio
+import json
 import random
-
-import pandas as pd
-import pickle
 
 from discord.ext import commands
 from discord import Embed
 
-PICKLE_FILE = 'ext/PlayerNicknames/NAMES.pickle'
+NICKNAME_FILE = 'ext/PlayerNicknames/NAMES.json'
 
 class PlayerNicknames(commands.Cog):
     """Player IDs"""
-    fields_formatted = ['Discord ID', 'Discord Name', 'Majsoul', 'Tenhou']
-    fields = ['discord_id', 'discord_name', 'mahjsoul_name', 'tenhou_name']
-    index = 'discord_id'
     def __init__(self, bot):
         self.bot = bot
-        self.players = None
+        self.players = {}
+        self.load()
 
-        self.initialize_database()
-
-    def initialize_database(self):
+    def load(self):
         try:
-            with open(PICKLE_FILE, 'rb') as f:
-                self.players = pickle.load(f)
+            with open(NICKNAME_FILE, 'r') as f:
+                self.players = json.load(f)
         except FileNotFoundError:
-            self.players = pd.DataFrame(columns=self.fields)
-            self.players.set_index(self.index, drop=True, inplace=True)
-            with open(PICKLE_FILE, 'wb') as f:
-                pickle.dump(self.players, f)
+            pass
     
     def save(self):
-        with open(PICKLE_FILE, 'wb') as f:
-            pickle.dump(self.players, f)
+        with open(NICKNAME_FILE, 'w') as f:
+            json.dump(self.players, f)
     
-    def check(self, user):
-        if user.id not in self.players['discord_id'].values:
-            self.players.loc[user.id] = [user.id, user.name, None, None]
-        
-        self.players['discord_name'][user.id] = user.name
+    def ensure(self, user):
+        player = self.players.get(user.id, {
+            'majsoul_name': None,
+            'tenhou_name': None,
+        })
+
+        player['discord_name'] = user.name
+        self.players[user.id] = player
     
     def exists(self, user):
         return user.id in self.players['discord_id'].values
@@ -52,18 +46,18 @@ class PlayerNicknames(commands.Cog):
         Displays the Majsoul and Tenhou names that have been registered for the Discord user.
         '''
 
-        self.check(ctx.author)
+        self.ensure(ctx.author)
+        self.save()
 
         embed = Embed(description="Registered account names:")
 
         embed.set_author(name=f"{ctx.author.name}", icon_url=ctx.author.avatar_url)
         embed.set_thumbnail(url=ctx.author.avatar_url)
 
-        embed.add_field(name="Majsoul", value=self.players['mahjsoul_name'][ctx.author.id])
-        embed.add_field(name="Tenhou", value=self.players['tenhou_name'][ctx.author.id])
+        embed.add_field(name="Majsoul", value=self.players[ctx.author.id]['majsoul_name'])
+        embed.add_field(name="Tenhou", value=self.players[ctx.author.id]['tenhou_name'])
 
         await ctx.send(embed=embed)
-        self.save()
 
     @commands.command(name='majsoul-name', aliases=['mahjsoul-name'])
     async def register_mahjsoul_name(self, ctx, name:str):
@@ -71,15 +65,16 @@ class PlayerNicknames(commands.Cog):
         
         Usage: `ms/majsoul-name <name>`
 
-        Links your majsoul name to your Discord account. This is a prerequisite for being able to use `ms/pause` or `ms/unpause`.
+        Links your majsoul name to your Discord account. This is a prerequisite
+        for being able to use `ms/pause` or `ms/unpause` without specifying
+        that name.
         '''
 
-        self.check(ctx.author)
-
-        self.players['mahjsoul_name'][ctx.author.id] = name
+        self.ensure(ctx.author)
+        self.players[ctx.author.id]['majsoul_name'] = name
+        self.save()
 
         await ctx.send(f'Majsoul nickname registered for {ctx.author.mention}.')
-        self.save()
     
     @commands.command(name='tenhou-name')
     async def register_tenhou_name(self, ctx, name:str):
@@ -90,18 +85,16 @@ class PlayerNicknames(commands.Cog):
         Linkes your tenhou name to your Discord account. Currently this has no use.
         '''
 
-        self.check(ctx.author)
-
-        self.players['tenhou_name'][ctx.author.id] = name
+        self.ensure(ctx.author)
+        self.players[ctx.author.id]['tenhou_name'] = name
+        self.save()
 
         await ctx.send(f'Tenhou nickname registered for {ctx.author.mention}.')
-        self.save()
     
     @commands.command(name='display-names', hidden=True)
     async def display_names(self, ctx):
         '''Displays all names.'''
         print(self.players)
-
 
 def setup(bot):
     bot.add_cog(PlayerNicknames(bot))
